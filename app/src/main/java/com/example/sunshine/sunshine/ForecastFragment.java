7a8ee8aa17f3d48571687679da65b86e8c87447b
salveta <salveta4_@hotmail.com>
2015-03-26 16:59:47 +0000

    add icons, two ui in tablet

--- app/src/main/java/com/example/sunshine/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/sunshine/sunshine/ForecastFragment.java
@@ -34,7 +34,8 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
             // On the one hand, that's annoying.  On the other, you can search the weather table
             // using the location set by the user, which is only in the Location table.
             // So the convenience is worth it.
-            WeatherContract.WeatherEntry.TABLE_NAME + "." + WeatherContract.WeatherEntry._ID,
+            WeatherContract.WeatherEntry.TABLE_NAME + "." +
+            WeatherContract.WeatherEntry._ID,
             WeatherContract.WeatherEntry.COLUMN_DATE,
             WeatherContract.WeatherEntry.COLUMN_SHORT_DESC,
             WeatherContract.WeatherEntry.COLUMN_MAX_TEMP,
